661f9cc870c57fb4e6046c5c099c1f62c37c7f01
daitor <tim90721@gmail.com>
2024-02-18 17:09:42 +0800

    Makefile: qemu: update run rule
    Update qemu RUN rule so that the build system will build the project first
    if the related settings, such as configurations or tools, or image are not
    built yet.
    
    Signed-off-by: daitor <tim90721@gmail.com>

--- scripts/qemu.mk
+++ scripts/qemu.mk
@@ -2,5 +2,3 @@ QFLAGS := -nographic -smp 1 -machine virt -bios none
 
 quiet_cmd_qemu_run =   RUN    $(patsubst $(TARGET_OUTDIR)/%,%,$(TARGET_ELF))
 cmd_qemu_run = $(QEMU) $(QFLAGS) -kernel $(TARGET_ELF)
-run: $(TARGET_ELF)
-	$(call cmd,qemu_run)
