db9a999b59dcf0b927cd5928c9468c45ee2605a0
daitor <tim90721@gmail.com>
2024-02-28 00:49:32 +0800

    arch: risc-v: platform: virt: initialize platform
    Add platform initialization function.
    Kernel will call a platform_init() function by default.
    
    Signed-off-by: daitor <tim90721@gmail.com>

---
+++ src/arch/risc-v/platform/virt/platform.c
@@ -0,0 +1,5 @@
+#include <platform/platform.h>
+
+int platform_init(void)
+{
+}
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <platform/platform.h>
 
 int start_kernel(void)
 {
+	int ret;
 
+	ret = platform_init();
+	if (ret)
+		exit(ret);
+
+	printf("hello kernel\n");
 
 	return 0;
 }
