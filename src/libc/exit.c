f4b8f8a93fea85c2e29423e9819b4f53ac00dcd4
daitor <tim90721@gmail.com>
2024-02-28 00:11:15 +0800

    libc: stdlib: add exit function
    Add an exit function.
    Since there isn't many way to recover kernel whenever an error is
    occurred, we can only spin it there forever.
    
    Signed-off-by: daitor <tim90721@gmail.com>

---
+++ src/libc/exit.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+void exit(s32 err)
+{
+	while (1)
+		;
+}
---
+++ src/libc/include/stdlib.h
@@ -0,0 +1,7 @@
+#ifndef _STDLIB_H_
+#define _STDLIB_H_
+
+#include <stdint.h>
+
+void exit(s32 err);
+#endif /* _STDLIB_H_ */
