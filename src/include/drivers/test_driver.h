12a6fe1e5348630bd6e3b1c31e268cf07cd60321
daitor <tim90721@gmail.com>
2024-02-28 00:15:34 +0800

    drivers: remove test_driver
    test_driver is only used to test build. It is no longer used anymore.
    
    Signed-off-by: daitor <tim90721@gmail.com>

--- src/include/drivers/test_driver.h
+++
@@ -1,4 +0,0 @@
-#ifndef _TEST_DRIVER_H_
-#define _TEST_DRIVER_H_
-int test_driver_hello(void);
-#endif /* _TEST_DRIVER_H_ */
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <drivers/test_driver.h>
 
 int start_kernel(void)
 {
-	//printf("%s hello\n", __func__);
 
-	test_driver_hello();
 
 	return 0;
 }
