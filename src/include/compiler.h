a5c3a440d7667abb3dfee7528e6e2f7b8cfb93b1
daitor <tim90721@gmail.com>
2024-04-10 00:56:47 +0800

    include: compiler: avoid offsetof built_in function redefinition error
    Since compiler may provide its implementation of offsetof macro and cause
    build error, add a preprocessor guard to prevent redefinition.
    
    Signed-off-by: daitor <tim90721@gmail.com>

--- src/include/compiler.h
+++ src/include/compiler.h
@@ -1,7 +1,10 @@
 #ifndef _COMPILER_H_
 #define _COMPILER_H_
 
+#ifndef offsetof
 #define offsetof(type, member)		((uintptr_t)&((type *)0)->member)
+#endif
+
 #define container_of(ptr, type, member)	({				\
 	const typeof(((type *)0)->member) *__mptr = (ptr);		\
 	(type *)((char *)__mptr - offsetof(type, member));		\
