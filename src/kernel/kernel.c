975f2b07338fe2a4f648422dc56e191613576092
daitor <tim90721@gmail.com>
2024-05-30 16:38:40 +0800

    kernel: add sample task
    Add a simple task for demonstration.
    
    Signed-off-by: daitor <tim90721@gmail.com>

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,9 +1,25 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <mm/mm.h>
+#include <sched.h>
 
 #include <platform/platform.h>
+
+void task_a(void *param)
+{
+	int i = 0;
+
+	while (1) {
+		printf("a start\n");
+
+		while (i < 10000000)
+			i++;
+
+		i = 0;
+	}
+}
 
 int start_kernel(void)
 {
@@ -16,6 +32,14 @@ int start_kernel(void)
 	memory_init();
 
 	printf("hello kernel\n");
+
+	ret = task_create(task_a, NULL, PAGE_SIZE);
+	if (ret) {
+		printf("create task failed: %d\n", ret);
+		return 0;
+	}
+
+	schedule();
 
 	return 0;
 }
