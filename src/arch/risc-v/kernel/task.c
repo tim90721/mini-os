ea31c36c07a4508fffe21d50058c72a7bf93bc35
daitor <tim90721@gmail.com>
2024-05-30 16:33:12 +0800

    risc-v: implement context switch
    Implement risc-v context swtich.
    
    During context switching, we preserve current context's stack pointer to
    mscratch so that we can backup context when the task is switching out.
    Also, this is much easier to implement context switch by using an
    additional spare register. However, it should be able to optimize the use
    of spare register.
    
    Signed-off-by: daitor <tim90721@gmail.com>

---
+++ src/arch/risc-v/kernel/task.c
@@ -0,0 +1,18 @@
+#include <compiler.h>
+#include <task.h>
+#include <types.h>
+
+void __task_create(struct task_struct *task,
+		   void (*task_func)(void *param), void *param, u32 stack_size)
+{
+	if (!task || !task_func)
+		return;
+
+	task->ctx.ra = (reg_t)task_func;
+	task->ctx.sp = (reg_t)((addr_t)task + sizeof(struct task_struct));
+
+	if (param)
+		task->ctx.a0 = (reg_t)param;
+	else
+		task->ctx.a0 = 0;
+}
