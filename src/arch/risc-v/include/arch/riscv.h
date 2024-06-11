707e7bbea7d3fe8ba20763835742e54a91977f33
daitor <tim90721@gmail.com>
2024-06-12 01:55:35 +0800

    risc-v: add MIE, MSTATUS CSR read/write APIs
    Add MIE, MSTATUS CSR read/write APIs. These APIs are essentials to
    implement interrupt handlers. MSTATUS controls global level interrupt
    enable/disable. MIE controls external and timer interrupt enable/disable.
    
    Signed-off-by: daitor <tim90721@gmail.com>

--- src/arch/risc-v/include/arch/riscv.h
+++ src/arch/risc-v/include/arch/riscv.h
@@ -4,11 +4,50 @@
 #include <bits.h>
 #include <types.h>
 
+/* MIE */
+#define MIE_SSIE		BIT(1)
+#define MIE_MSIE		BIT(3)
+#define MIE_STIE		BIT(5)
+#define MIE_MTIE		BIT(7)
+#define MIE_SEIE		BIT(9)
+#define MIE_MEIE		BIT(11)
+
+/* MSTATUS */
+#define MSTATUS_MIE		BIT(3)
+
 /* MCAUSE */
 #define MCAUSE_IRQ		BIT(31)
 
 static inline void set_mtvec(void (*handler)(void))
 {
 	__asm__ __volatile__("csrw mtvec, %0" : : "r"(handler));
+}
+
+static inline void mie_write(reg_t val)
+{
+	__asm__ __volatile__("csrw mie, %0" : : "r"(val));
+}
+
+static inline reg_t mie_read(void)
+{
+	reg_t val;
+
+	__asm__ __volatile__("csrr %0, mie" : "=r"(val));
+
+	return val;
+}
+
+static inline void mstatus_write(reg_t val)
+{
+	__asm__ __volatile__("csrw mstatus, %0" : : "r"(val));
+}
+
+static inline reg_t mstatus_read(void)
+{
+	reg_t val;
+
+	__asm__ __volatile__("csrr %0, mstatus" : "=r"(val));
+
+	return val;
 }
 #endif /* _RISC_V_ */
