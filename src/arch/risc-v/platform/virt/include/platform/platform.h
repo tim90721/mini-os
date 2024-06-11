343eecb423f507629138a27c15363a755c03151e
daitor <tim90721@gmail.com>
2024-06-12 01:53:28 +0800

    risc-v: virt: add PLIC driver platform implementation
    Implement risc-v virt platform level PLIC driver.
    
    Signed-off-by: daitor <tim90721@gmail.com>

--- src/arch/risc-v/platform/virt/include/platform/platform.h
+++ src/arch/risc-v/platform/virt/include/platform/platform.h
@@ -1,6 +1,7 @@
 #ifndef _PLATFORM_H_
 #define _PLATFORM_H_
 
+#define PLIC_BASE	0x0C000000
 #define UART_BASE	0x10000000
 
 int platform_init(void);
--- src/arch/risc-v/platform/virt/platform.c
+++ src/arch/risc-v/platform/virt/platform.c
@@ -1,11 +1,16 @@
 #include <arch/trap.h>
 #include <platform/platform.h>
 
+#include <drivers/irqchip/irqchip-plic.h>
 #include <drivers/uart/uart_16550A.h>
 
 int platform_init(void)
 {
 	int ret;
+
+	ret = plic_init();
+	if (ret)
+		return ret;
 
 	ret = trap_init();
 	if (ret)
