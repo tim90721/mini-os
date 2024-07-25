44845bfe40a1e2b7739d2689c4551b44dde1ddcd
daitor <tim90721@gmail.com>
2024-07-25 23:41:37 +0800

    kernel: add architecture dependent timer IRQ handler
    Add support to handle architecture dependent timer IRQ handler.
    The handler should be responsible to update global tick.
    
    Signed-off-by: daitor <tim90721@gmail.com>

--- src/include/interrupt.h
+++ src/include/interrupt.h
@@ -4,7 +4,9 @@
 #include <irq.h>
 
 extern void (*arch_irq_handler)(void);
+extern void (*arch_timer_handler)(void);
 
 int unused_irq_handler(void *priv);
 int set_arch_irq_handler(void (*irq_handler)(void));
+int set_arch_timer_handler(void (*timer_handler)(void));
 #endif /* _INTERRUPT_H_ */
