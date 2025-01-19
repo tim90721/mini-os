90f61a7c1c53e5908c481f39385c0d74eec8f8e6
tim90721 <tim90721@gmail.com>
2025-01-19 21:51:24 +0800

    libc: extend stdint to support different architecture
    Since different architecture has different data types, make libc include
    architecture dependent stdint.h for their definitions.
    
    Signed-off-by: tim90721 <tim90721@gmail.com>

--- src/libc/include/stdint.h
+++ src/libc/include/stdint.h
@@ -1,27 +1,15 @@
 #ifndef _STDINT_H_
 #define _STDINT_H_
 
-typedef unsigned long uintptr_t;
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+#include <arch/stdint.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef signed long long int64_t;
-
 typedef int8_t s8;
 typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
-
-typedef u32 size_t;
 #endif /* _STDINT_H_ */
