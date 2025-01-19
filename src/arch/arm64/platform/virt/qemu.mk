a16000374bb68493430df6fde459f7acd0cc2dd0
tim90721 <tim90721@gmail.com>
2025-01-19 22:12:35 +0800

    arm64: virt: add configuration to select number of CPUs for the project
    Add a configuration to select 1, 2, 4, 8 processors for the ARM64 virt
    platform.
    
    Signed-off-by: tim90721 <tim90721@gmail.com>

--- src/arch/arm64/platform/virt/qemu.mk
+++ src/arch/arm64/platform/virt/qemu.mk
@@ -2,10 +2,9 @@
 
 qflags-y += -machine virt
 qflags-y += -cpu cortex-a57
-qflags-y += -machine type=virt
 qflags-y += -nographic
 qflags-y += -m 2048
-qflags-y += -smp 2
+qflags-y += -smp $(CONFIG_ARM64_VIRT_CPU_CORES)
 
 qflags-$(CONFIG_ARM64_HYPERVISOR) += -machine virtualization=on
 qflags-$(CONFIG_ARM64_TRUSTZONE) += -machine secure=on
