2f53c41149907d9f7708ea0a8243079babe5fc93
tim90721 <tim90721@gmail.com>
2025-02-13 00:02:34 +0800

    arm64: virt: initialize GIC interrupt controller
    Enable GIC interrupt controller for Arm64 virt platform.
    
    Signed-off-by: tim90721 <tim90721@gmail.com>

--- src/arch/arm64/platform/virt/include/platform/platform.h
+++ src/arch/arm64/platform/virt/include/platform/platform.h
@@ -1,7 +1,12 @@
 #ifndef _PLATFORM_H_
 #define _PLATFORM_H_
 
-#define UART_BASE	0x9000000
+#define GIC_BASE_DIST	0x08000000
+#define GIC_BASE_CPU	0x08010000
+#define GIC_BASE_V2M	0x08020000
+#define GIC_BASE_HYP	0x08030000
+#define GIC_BASE_VCPU	0x08040000
+#define UART_BASE	0x09000000
 
 int platform_init(void);
 #endif /* _PLATFORM_H_ */
--- src/arch/arm64/platform/virt/platform.c
+++ src/arch/arm64/platform/virt/platform.c
@@ -1,15 +1,21 @@
-#include <platform/platform.h>
+#include <compiler.h>
 
+#include <drivers/irqchip/irqchip-gic-v2.h>
 #include <drivers/uart/uart_pl011.h>
+
+#include <platform/platform.h>
 
 int platform_init(void)
 {
 	int ret;
 
+	ret = gic_init();
+	if (unlikely(ret))
+		return ret;
+
 	ret = uart_pl011_init();
-	if (ret)
+	if (unlikely(ret))
 		return ret;
 
 	return 0;
 }
-
