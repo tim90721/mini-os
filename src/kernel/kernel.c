44a837b0e0b0ea3bb292cfad6603626b86a75374
tim90721 <tim90721@gmail.com>
2025-03-07 00:31:41 +0800

    kernel: update kernel main with scheduler and idle task
    Update kernel main process.
    Add task2 to demonstrate the scheduler is able to work and run 2 different
    tasks with a configured delay.
    The kernel main function is used as idle task after the initialization are
    completed.
    
    Signed-off-by: tim90721 <tim90721@gmail.com>

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,3 +1,4 @@
+#include <compiler.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,22 +9,21 @@
 
 #include <platform/platform.h>
 
-static void init_task(void *param)
+static void task1(void *param)
 {
-	int i = 0;
-	int ret;
-
-	ret = platform_init();
-	if (ret)
-		exit(ret);
-
 	while (1) {
-		printf("task a running, tick: %u\n", tick);
+		printf("task1 running, tick: %u\n", tick);
 
-		while (i < 100000000)
-			i++;
+		msleep(1000);
+	}
+}
 
-		i = 0;
+static void task2(void *param)
+{
+	while (1) {
+		printf("task2 running, tick: %u\n", tick);
+
+		msleep(5000);
 	}
 }
 
@@ -32,22 +32,39 @@ int start_kernel(void)
 	int ret;
 
 	ret = memory_init();
-	if (ret)
+	if (unlikely(ret))
+		exit(ret);
+
+	ret = platform_init();
+	if (unlikely(ret))
 		exit(ret);
 
 	ret = time_init();
-	if (ret)
+	if (unlikely(ret))
 		exit(ret);
 
-	printf("hello kernel\n");
+	ret = sched_init();
+	if (unlikely(ret))
+		exit(ret);
 
-	ret = task_create(init_task, NULL, PAGE_SIZE);
+	ret = task_create(task1, NULL, PAGE_SIZE);
 	if (ret) {
 		printf("create init task failed: %d\n", ret);
 		return ret;
 	}
 
+	ret = task_create(task2, NULL, PAGE_SIZE);
+	if (ret) {
+		printf("create task2 failed: %d\n", ret);
+		return ret;
+	}
+
 	schedule();
+
+	while (1) {
+		if (sched_has_task_woken())
+			schedule();
+	}
 
 	return 0;
 }
